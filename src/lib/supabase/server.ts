570423238c1622fb60f8d90b52fd18a4084d091d
yunusdemirbag <ynsdemirbag@gmail.com>
2025-05-23 20:20:30 +0300

    fix: server.ts dosyası eklendi

---
+++ src/lib/supabase/server.ts
@@ -0,0 +1,1 @@
+import { createServerClient } from "@supabase/ssr"; import { cookies } from "next/headers"; export function createClient(cookiesInstance = cookies()) { return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, { cookies: { get(name) { return cookiesInstance.get(name)?.value; }, }, }); }
